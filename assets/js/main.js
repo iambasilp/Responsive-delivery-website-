f9ec5764903d066118441879094c2bba75592a7d
iambasilp <basilxg321@gmail.com>
2022-12-02 22:51:45 +0530

    Scroll Active link added

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,6 +26,27 @@ navLinks.forEach((navLink) => {
   navLink.addEventListener("click", showNavlink);
 });
 
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+console.log(sections)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 50,
+              sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 // Change header backround
 const scrollHeader = () => {
   const header = document.getElementById("header");
